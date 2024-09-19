68270aaa797913bccb5370fac3cc5aa9b4ee2f93
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-19 21:36:56 +0530

    UI completed

--- lib/view/registerview.dart
+++ lib/view/registerview.dart
@@ -10,6 +10,7 @@ class Registerview extends StatefulWidget {
 class _RegisterviewState extends State<Registerview> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         actions: const [
@@ -30,7 +31,7 @@ class _RegisterviewState extends State<Registerview> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Name",
+              "Student Name",
               style: TextStyle(
                   color: Colors.grey[500],
                   fontSize: 22,
@@ -42,7 +43,7 @@ class _RegisterviewState extends State<Registerview> {
                 Expanded(
                   child: TextFormField(
                     decoration: const InputDecoration(
-                        labelText: "First Name",
+                        labelText: "first Name",
                         labelStyle: TextStyle(fontWeight: FontWeight.w400),
                         border: OutlineInputBorder()),
                   ),
@@ -51,7 +52,7 @@ class _RegisterviewState extends State<Registerview> {
                 Expanded(
                   child: TextFormField(
                     decoration: const InputDecoration(
-                        labelText: "Last Name", border: OutlineInputBorder()),
+                        labelText: "last Name", border: OutlineInputBorder()),
                   ),
                 ),
               ],
@@ -73,6 +74,69 @@ class _RegisterviewState extends State<Registerview> {
               decoration: const InputDecoration(
                   labelText: "email", border: OutlineInputBorder()),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Class",
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(
+                  width: 150,
+                ),
+                Text(
+                  "Age",
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: "enter class",
+                        labelStyle: TextStyle(fontWeight: FontWeight.w400),
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: "enter age", border: OutlineInputBorder()),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: SizedBox(
+                width: size.width,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        shape: const RoundedRectangleBorder()),
+                    onPressed: () {},
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    )),
+              ),
+            )
           ],
         ),
       ),
