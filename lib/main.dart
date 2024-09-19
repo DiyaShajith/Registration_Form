9fff55b1134c6f43145ff7a7f583891d21f56d27
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-19 21:25:53 +0530

    UI started

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,brightness: Brightness.dark),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple, brightness: Brightness.dark),
         useMaterial3: true,
       ),
-      home: Registerview(),
+      home: const Registerview(),
     );
   }
 }
-
--- lib/view/registerview.dart
+++ lib/view/registerview.dart
@@ -10,6 +10,72 @@ class Registerview extends StatefulWidget {
 class _RegisterviewState extends State<Registerview> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          Icon(
+            Icons.person,
+            size: 32,
+          )
+        ],
+        title: const Text(
+          "Registration Form",
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Name",
+              style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: "First Name",
+                        labelStyle: TextStyle(fontWeight: FontWeight.w400),
+                        border: OutlineInputBorder()),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        labelText: "Last Name", border: OutlineInputBorder()),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Email",
+              style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "email", border: OutlineInputBorder()),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
