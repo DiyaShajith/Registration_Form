470b5a60d1e8d7bc68bba375969d58af051c7dce
Diya Shajith K <diyashajith.2231@gmail.com>
2024-09-20 07:16:32 +0530

    Registerform validation completed

--- lib/view/registerview.dart
+++ lib/view/registerview.dart
@@ -11,6 +11,7 @@ class _RegisterviewState extends State<Registerview> {
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _classController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
 
@@ -21,6 +22,7 @@ class _RegisterviewState extends State<Registerview> {
     _firstNameController.dispose();
     _lastNameController.dispose();
     _emailController.dispose();
+    _passwordController.dispose();
     _classController.dispose();
     _ageController.dispose();
     super.dispose();
@@ -48,157 +50,184 @@ class _RegisterviewState extends State<Registerview> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Student Name",
-                style: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.name,
-                      controller: _firstNameController,
-                      decoration: const InputDecoration(
-                        labelText: "First Name",
-                        labelStyle: TextStyle(fontWeight: FontWeight.w400),
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter first name';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.name,
-                      controller: _lastNameController,
-                      decoration: const InputDecoration(
-                          labelText: "Last Name", border: OutlineInputBorder()),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter last name';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Text(
-                "Email",
-                style: TextStyle(
-                    color: Colors.grey[500],
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                    labelText: "Email", border: OutlineInputBorder()),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an email';
-                  }
-
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Text(
-                    "Class",
-                    style: TextStyle(
-                        color: Colors.grey[500],
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(width: 150),
-                  Text(
-                    "Age",
-                    style: TextStyle(
-                        color: Colors.grey[500],
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      controller: _classController,
-                      decoration: const InputDecoration(
-                          labelText: "Enter class",
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Student Name",
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        keyboardType: TextInputType.name,
+                        controller: _firstNameController,
+                        decoration: const InputDecoration(
+                          labelText: "First Name",
                           labelStyle: TextStyle(fontWeight: FontWeight.w400),
-                          border: OutlineInputBorder()),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter class';
-                        }
-                        return null;
-                      },
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter first name';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      controller: _ageController,
-                      decoration: const InputDecoration(
-                          labelText: "Enter age", border: OutlineInputBorder()),
-                      keyboardType: TextInputType.number,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter age';
-                        }
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        keyboardType: TextInputType.name,
+                        controller: _lastNameController,
+                        decoration: const InputDecoration(
+                            labelText: "Last Name",
+                            border: OutlineInputBorder()),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter last name';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Email",
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      labelText: "Email", border: OutlineInputBorder()),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an email';
+                    }
 
-                        return null;
-                      },
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Password",
+                  style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  obscureText: true,
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                      labelText: "password", border: OutlineInputBorder()),
+                  keyboardType: TextInputType.visiblePassword,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an strong password';
+                    }
+
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text(
+                      "Class",
+                      style: TextStyle(
+                          color: Colors.grey[500],
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 50),
-              Center(
-                child: SizedBox(
-                  width: size.width,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey,
-                        shape: const RoundedRectangleBorder()),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Registration Successful'),
-                          ),
-                        );
-                      }
-                    },
-                    child: const Text(
-                      "Register",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    const SizedBox(width: 150),
+                    Text(
+                      "Age",
+                      style: TextStyle(
+                          color: Colors.grey[500],
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: _classController,
+                        decoration: const InputDecoration(
+                            labelText: "Enter class",
+                            labelStyle: TextStyle(fontWeight: FontWeight.w400),
+                            border: OutlineInputBorder()),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter class';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _ageController,
+                        decoration: const InputDecoration(
+                            labelText: "Enter age",
+                            border: OutlineInputBorder()),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter age';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Center(
+                  child: SizedBox(
+                    width: size.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          shape: const RoundedRectangleBorder()),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Registration Successful'),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
